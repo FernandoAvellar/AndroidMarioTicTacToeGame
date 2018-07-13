ecb2dd92e03a8a91c89c8ef02720fc4429e28095
Fernando Avellar <favellar@yahoo.com.br>
2018-07-13 10:04:33 -0300

    NewPicturesAndColors

--- app/src/main/java/com/example/connect3game/MainActivity.java
+++ app/src/main/java/com/example/connect3game/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    //0:Koopa 1:Mario 2:Empty
+    //0:Bowser 1:Mario 2:Empty
     int activePlayer = 0;
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2}; //start all 9 slots with empty representation
     int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}}; //possibilities to win the game
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
                 counter.setImageResource(R.drawable.mario);
                 activePlayer = 1;
             } else {
-                counter.setImageResource(R.drawable.koopa);
+                counter.setImageResource(R.drawable.bowser);
                 activePlayer = 0;
             }
             counter.animate().translationYBy(1500).rotation(3600).setDuration(250);
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
                 if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
                     // Someone has won!
                     gameActive = false;
-                    String winner = activePlayer == 1 ? "Mario" : "Koopa";
+                    String winner = activePlayer == 1 ? "Mario" : "Bowser";
                     Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
                     TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
                     winnerTextView.setText(winner + " has won!");
