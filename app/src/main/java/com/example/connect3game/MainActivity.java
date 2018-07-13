b27fcae07f921f3f116d6191e2cd64df35678e0f
Fernando Avellar <favellar@yahoo.com.br>
2018-07-13 09:43:58 -0300

    Identation and comments

--- app/src/main/java/com/example/connect3game/MainActivity.java
+++ app/src/main/java/com/example/connect3game/MainActivity.java
@@ -11,8 +11,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     //0:Koopa 1:Mario 2:Empty
     int activePlayer = 0;
-    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
-    int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2}; //start all 9 slots with empty representation
+    int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}}; //possibilities to win the game
     boolean gameActive = true;
     int numberOfAttempts = 0;
 
@@ -30,33 +30,32 @@ public class MainActivity extends AppCompatActivity {
             winnerTextView.setVisibility(View.VISIBLE);
         }
 
-            if (gameState[tappedCounter] == 2 && gameActive) {
+        if (gameState[tappedCounter] == 2 && gameActive) {
+            gameState[tappedCounter] = activePlayer;
+            counter.setTranslationY(-1500);
 
-                gameState[tappedCounter] = activePlayer;
-                counter.setTranslationY(-1500);
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.mario);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.koopa);
+                activePlayer = 0;
+            }
+            counter.animate().translationYBy(1500).rotation(3600).setDuration(250);
 
-                if (activePlayer == 0) {
-                    counter.setImageResource(R.drawable.mario);
-                    activePlayer = 1;
-                } else {
-                    counter.setImageResource(R.drawable.koopa);
-                    activePlayer = 0;
-                }
-                counter.animate().translationYBy(1500).rotation(3600).setDuration(250);
-
-                for (int[] winningPosition : winningPositions) {
-                    if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
-                        // Somone has won!
-                        gameActive = false;
-                        String winner = activePlayer == 1 ? "Mario" : "Koopa";
-                        Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
-                        TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
-                        winnerTextView.setText(winner + " has won!");
-                        playAgainButton.setVisibility(View.VISIBLE);
-                        winnerTextView.setVisibility(View.VISIBLE);
-                    }
+            for (int[] winningPosition : winningPositions) {
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
+                    // Someone has won!
+                    gameActive = false;
+                    String winner = activePlayer == 1 ? "Mario" : "Koopa";
+                    Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
+                    TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+                    winnerTextView.setText(winner + " has won!");
+                    playAgainButton.setVisibility(View.VISIBLE);
+                    winnerTextView.setVisibility(View.VISIBLE);
                 }
             }
+        }
     }
 
     public void playAgain(View view) {
